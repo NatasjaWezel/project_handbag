04a485a78ef48ebae84084aad5677f48f0422b64
Natasja Wezel <natasjawezel@hotmail.com>
2020-09-22 10:21:41 +0200

    Added distance to closest atom and that atoms vdw radius

--- code/plot_all_contact_atoms.py
+++ code/plot_all_contact_atoms.py
@@ -7,6 +7,9 @@
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
 from helpers.geometry_helpers import average_fragment, calculate_center
+from helpers.plot_functions import plot_fragment_colored
+
+import math
 
 import pandas as pd
 import numpy as np
@@ -20,81 +23,96 @@ import sys
 
 def main():
 
-    if len(sys.argv) != 2:
-        print("Usage: python plot_all_contact_atoms.py <path/to/inputfile>")
+    if len(sys.argv) != 3:
+        print("Usage: python plot_all_contact_atoms.py <path/to/inputfile> <atom to count or center>")
         sys.exit(1)
     
     inputfilename = sys.argv[1]
+    to_count = sys.argv[2]
 
     prefix = inputfilename.rsplit("/\\", 1)[-1].rsplit(".", 1)[0] 
     avg_fragment_name = prefix + "_avg_fragment.pkl"
-
-    # TODO: make this more general:
-    # to_count = ["O"]
-    to_count = ["center"]
 
     fragments_df = pd.read_csv(inputfilename, header=None)
     fragments_df.columns = ["entry_id", "fragment_id", "atom_label", "atom_symbol", "fragment_or_contact", "atom_x", "atom_y", "atom_z"]
 
     avg_fragment = average_fragment(avg_fragment_name, fragments_df)
 
-    coordinate_lists = count_contact_atoms(fragments_df, to_count)
+    coordinate_df = count_contact_atoms(fragments_df, to_count)
 
-    make_plot(avg_fragment, coordinate_lists)
+    coordinate_df = distances_closest_atom_central(coordinate_df, avg_fragment)
+
+    make_plot(avg_fragment, coordinate_df)
+
+def distances_closest_atom_central(coordinate_df, avg_fragment):
+    closest_distances = []
+    closest_atoms = []
+    
+    for x, y, z in zip(coordinate_df.x, coordinate_df.y, coordinate_df.z):
+        closest_distance = math.inf
+
+        for atom in avg_fragment.atoms.values():
+            distance = np.sqrt((x - atom.x)**2 + (y - atom.y)**2 + (z - atom.z)**2)
+            
+            if distance < closest_distance:
+                closest_distance = distance
+                closest_atom = atom.label
+                
+        closest_distances.append(closest_distance)
+        closest_atoms.append(closest_atom)
+
+    coordinate_df["distance"] = closest_distances
+    coordinate_df["vdw_closest_atom"] = closest_atoms
+
+    return coordinate_df
 
 def count_contact_atoms(fragments_df, to_count):
     """ This is a function that counts the contact atoms or centers of contact groups near the
         central group. """
 
-    Xs, Ys, Zs = [], [], []
-
     contact_group_df = fragments_df[fragments_df.fragment_or_contact == "f"]
 
-    for entry_id in contact_group_df.entry_id.unique():
+    labels = contact_group_df.entry_id.unique()
+    coordinate_df = pd.DataFrame(columns=["x", "y", "z", "distance", "vdw_closest_atom"], index=labels)
+
+    for entry_id in labels:
         entry_df = contact_group_df[contact_group_df.entry_id == entry_id]
 
         for fragment_id in entry_df.fragment_id.unique():
             fragment_df = entry_df[entry_df.fragment_id == fragment_id]
             
-            # TODO: this will break if to_count is more than a single atom
-            for column in to_count:
-                # if center, calculate per fragment instead of per atom
-                if "center" == column:
-                    coordinates = calculate_center(fragment_df=fragment_df, atoms=["C"])
-                else:
-                    point = fragment_df[fragment_df['atom_label'].str.contains(column)]
+            coordinates = get_coordinates(fragment_df, to_count)
+                
+            coordinate_df.loc[coordinate_df.index == entry_id, ["x", "y", "z"]] = coordinates[0], coordinates[1], coordinates[2]
 
-                    assert (len(point) == 1), " atom label is not unique, can't count per bin"
+    return coordinate_df
 
-                    coordinates = [point.atom_x, point.atom_y, point.atom_z]
+def get_coordinates(fragment_df, to_count):
+    # if center, calculate per fragment instead of per atom
+    if to_count == "center":
+        return calculate_center(fragment_df=fragment_df, atoms=["C"])
+    else:
+        point = fragment_df[fragment_df['atom_label'].str.contains(to_count)]
 
-                Xs.append(coordinates[0])
-                Ys.append(coordinates[1])
-                Zs.append(coordinates[2])
+        assert (len(point) == 1), " atom label is not unique, can't count per bin"
+        
+        return [float(point.atom_x), float(point.atom_y), float(point.atom_z)]
 
-    return [Xs, Ys, Zs]
-
-
-def make_plot(avg_fragment, coordinate_lists):
+def make_plot(avg_fragment, coordinate_df):
     """ Plot all the surrounding contact groups around the central group. """
 
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
     
     # plot the (average of the) central group 
-    for atom in avg_fragment.atoms.values():
-        if "O" in atom.label:
-            ax.scatter(atom.x, atom.y, atom.z, c="red", s=20, edgecolor="black")
-        elif "N" in atom.label:
-            ax.scatter(atom.x, atom.y, atom.z, c="blue", s=20, edgecolor="black")
+    ax = plot_fragment_colored(ax, avg_fragment)
     
-    ax.scatter(coordinate_lists[0], coordinate_lists[1], coordinate_lists[2], s=1, c="red")
+    ax.scatter(coordinate_df.x, coordinate_df.y, coordinate_df.z, s=1, c="red")
 
     ax.set_xlabel('X axis')
     ax.set_ylabel('Y axis')
     ax.set_zlabel('Z axis')
 
-    plt.title("Central group and all contact atoms")
     plt.show()
 
 
