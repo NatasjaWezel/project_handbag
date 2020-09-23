b9a87b83cb23dc0d679d501070a0bc2e69b80fe5
Natasja Wezel <natasjawezel@hotmail.com>
2020-09-23 13:34:12 +0200

    Define vdw_radii csv in header

--- code/helpers/headers.py
+++ code/helpers/headers.py
@@ -1,4 +1,5 @@
 CUT_OFF_ZERO = 1e-10
 AXCOLOR = 'lightgoldenrodyellow'
 
+VDW_CSV = ".\data\\vdw_radii.csv"
 COLORS = ["red", "sandybrown", "gold", "chartreuse", "green", "mediumturquoise", "dodgerblue", "darkblue", "slateblue", "mediumorchid", "fuchsia"]
