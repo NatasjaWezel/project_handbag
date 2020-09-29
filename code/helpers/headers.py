cccfcb5e21a9e215e68a5a3080703205f92f1617
Natasja Wezel <natasjawezel@hotmail.com>
2020-09-29 17:06:58 +0200

    add directories to headers

--- code/helpers/headers.py
+++ code/helpers/headers.py
@@ -1,5 +1,7 @@
 CUT_OFF_ZERO = 1e-10
 AXCOLOR = 'lightgoldenrodyellow'
 
-VDW_CSV = ".\data\\vdw_radii.csv"
+DATADIR = ".\\data\\"
+RESULTSDIR = ".\\results\\"
+RADII_CSV = ".\\data\\radii.csv"
 COLORS = ["red", "sandybrown", "gold", "chartreuse", "green", "mediumturquoise", "dodgerblue", "darkblue", "slateblue", "mediumorchid", "fuchsia"]
