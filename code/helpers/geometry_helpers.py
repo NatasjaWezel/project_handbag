b1a23a57b00428f26b21ca8dce998d456683c3c0
Natasja Wezel <natasjawezel@hotmail.com>
2020-10-01 16:28:19 +0200

    Change average fragment helper

--- code/helpers/geometry_helpers.py
+++ code/helpers/geometry_helpers.py
@@ -6,7 +6,10 @@ import math
 
 import pickle
 
+import time
 from tqdm import tqdm
+
+import csv
 
 def calculate_center(fragment_df, atoms):
     frames = []
@@ -20,62 +23,62 @@ def calculate_center(fragment_df, atoms):
 
     return coordinates
 
-def average_fragment(settings, df):
-    """ Returns a fragment containing the bonds and average points of the interacting central groups. 
-        # TODO: its NO3 specific right now """ 
 
+def make_avg_fragment_if_not_exists(settings, df):
     try:
         openpicklefile = open(settings.get_avg_fragment_filename(), 'rb')
         fragment = pickle.load(openpicklefile)
         openpicklefile.close()
+
+        return fragment
     except FileNotFoundError:
-        central_group_df = df[df.in_central_group]
-
-        columns = []
-        atoms = []
-
-        count_dict = {}
-
-        for atom, amount in settings.central_group_atoms.items():
-            count_dict[atom] = 1
-
-            for i in range(1, amount + 1):
-                atoms.append(atom + str(i))
-                columns.extend([atom + str(i) + "x", atom + str(i) + "y", atom + str(i) + "z"])
-
-        # count how many atoms in one fragment
-        new_df = pd.DataFrame(columns=columns, index=central_group_df.id.unique())
-
-        # put first fragment in there
-        label = central_group_df.id.unique()[0]
-        single_fragment_df = central_group_df[central_group_df.id == label]
-
-        closest = {}
-        for _, row in single_fragment_df.iterrows():
-            number = count_dict[row.atom_symbol]
-
-            new_df = add_coordinates_to_df(new_df, label, row.atom_symbol + str(number), row)
-
-            closest[row.atom_symbol + str(number)] = [row.atom_x, row.atom_y, row.atom_z]
-
-            count_dict[row.atom_symbol] += 1
-
-        # put rest of fragments in the new df
-        new_df = fill_coordinates(central_group_df, new_df, closest)
+        fragment = average_fragment(settings, df)
         
-        fragment = make_fragment(atoms, new_df)
-
         openpicklefile = open(settings.get_avg_fragment_filename(), 'wb')
         pickle.dump(fragment, openpicklefile)
         openpicklefile.close()
-            
+
+        return fragment
+
+
+def average_fragment(settings, df):
+    """ Returns a fragment containing the average points of the central groups. """ 
+    
+    central_group_df = df[df.in_central_group]
+
+    columns, atoms, count_dict = settings.get_avg_fragment_helpers()
+
+    columns.append('time')
+
+    # count how many atoms in one fragment
+    new_df = pd.DataFrame(columns=columns, index=central_group_df.id.unique())
+
+    # put coordinates in new df
+    new_df = fill_coordinates(central_group_df, new_df, count_dict)
+
+    # save it for stats
+    new_df = new_df.apply(pd.to_numeric, downcast='float', errors='coerce')
+    new_df.to_hdf(settings.get_avg_fragment_hdf_filename(), 'key')
+
+    fragment = make_fragment(atoms, new_df)
+
     return fragment   
 
 
-def fill_coordinates(central_group_df, new_df, closest):
-    # TODO: time this and check std's to see what's worth and what's not
-    labels = central_group_df.id.unique()[1:]
-    
+def fill_coordinates(central_group_df, new_df, count_dict):
+    labels = central_group_df.id.unique()
+
+    # put first fragment in there
+    label = labels[0]
+    single_fragment_df = central_group_df[central_group_df.id == label]
+
+    closest = {}
+    for _, row in single_fragment_df.iterrows():
+        number = count_dict[row.atom_symbol]
+        new_df = add_coordinates_to_df(new_df, label, row.atom_symbol + str(number), row)
+        closest[row.atom_symbol + str(number)] = [row.atom_x, row.atom_y, row.atom_z]
+        count_dict[row.atom_symbol] += 1
+
     print("Calculating average fragment: ")
     for label in tqdm(labels):
         single_fragment_df = central_group_df[central_group_df.id == label]
@@ -93,8 +96,11 @@ def fill_coordinates(central_group_df, new_df, closest):
                     closest_atom = key
 
             new_df = add_coordinates_to_df(df=new_df, label=label, atom_symbol=closest_atom, row=row)
+            new_df['time'] = time.time()
 
-    return new_df
+    dropped_na = new_df.dropna()
+    print("Dropping", len(new_df) - len(dropped_na), "fragments for avg due to NaN values")
+    return dropped_na
 
 def make_fragment(atoms, new_df):
     fragment = Fragment(from_entry="allentries", fragment_id=1)
@@ -110,8 +116,6 @@ def make_fragment(atoms, new_df):
 
 
 def add_coordinates_to_df(df, label, atom_symbol, row):
-    if label == "FOYMIS1":
-        print(df.loc[df.index == label], atom_symbol)
     df.loc[df.index == label, atom_symbol + "x"] = row.atom_x
     df.loc[df.index == label, atom_symbol + "y"] = row.atom_y
     df.loc[df.index == label, atom_symbol + "z"] = row.atom_z
