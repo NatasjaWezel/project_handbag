7c91c13bc9f38784926bde8e10fcd4ac3dfb863a
Natasja Wezel <natasjawezel@hotmail.com>
2020-10-16 16:21:41 +0200

    Separated calculationand plotting of avg fragment

--- code/2a_plot_avg_fragment.py
+++ code/2a_plot_avg_fragment.py
@@ -3,6 +3,10 @@ import sys
 from classes.Settings import Settings
 from helpers.geometry_helpers import make_avg_fragment_if_not_exists
 from helpers.helpers import read_results_alignment
+from helpers.plot_functions import plot_fragment_colored
+
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
 
 def main():
@@ -17,7 +21,19 @@ def main():
 
     aligned_fragments_df = read_results_alignment(settings.get_aligned_csv_filename())
 
-    make_avg_fragment_if_not_exists(settings, aligned_fragments_df)
+    fragment = make_avg_fragment_if_not_exists(settings, aligned_fragments_df)
+
+    fig = plt.figure()
+    ax: Axes3D = fig.add_subplot(111, projection='3d')
+
+    # plot the (average of the) central group
+    ax = plot_fragment_colored(ax, fragment)
+
+    ax.set_xlabel('X axis')
+    ax.set_ylabel('Y axis')
+    ax.set_zlabel('Z axis')
+
+    plt.show()
 
 
 if __name__ == "__main__":
