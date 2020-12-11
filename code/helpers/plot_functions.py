a6b79569c27aaf671d5cbe9d1869077a44c58eb9
Natasja Wezel <natasjawezel@hotmail.com>
2020-12-11 15:49:49 +0100

    compatibility, add lower_lim to plot_density

--- code/helpers/plot_functions.py
+++ code/helpers/plot_functions.py
@@ -7,22 +7,22 @@ from mpl_toolkits.mplot3d import Axes3D
 from helpers.headers import COLORS
 
 
-def plot_density(ax, df, settings):
+def plot_density(ax, df, settings, lower_lim=0.0001):
     df['ymiddle'] = (df['ystart'] * 2 + settings.resolution) / 2
     df['xmiddle'] = (df['xstart'] * 2 + settings.resolution) / 2
     df['zmiddle'] = (df['zstart'] * 2 + settings.resolution) / 2
 
-    # normalize per column
-    df[settings.to_count_contact] = df[settings.to_count_contact] / df[settings.to_count_contact].sum()
+    # normalize column
+    df[settings.to_count_contact + "_normalized"] = df[settings.to_count_contact] / df[settings.to_count_contact].sum()
 
-    points = df[df[settings.to_count_contact] > 0.00001]
+    points = df[df[settings.to_count_contact + "_normalized"] > lower_lim]
 
-    norm = plt.Normalize(0.00001, points[settings.to_count_contact].max())
+    norm = plt.Normalize(lower_lim, points[settings.to_count_contact + "_normalized"].max())
     cmap = matplotlib.colors.LinearSegmentedColormap.from_list("", ["lightblue", "fuchsia", "red"])
 
     p = ax.scatter(list(points.xmiddle), list(points.ymiddle), list(points.zmiddle),
-                   s=list(10000 * points[settings.to_count_contact]),
-                   c=list(points[settings.to_count_contact]),
+                   s=list(10000 * points[settings.to_count_contact + "_normalized"]),
+                   c=list(points[settings.to_count_contact + "_normalized"]),
                    cmap=cmap,
                    norm=norm)
 
@@ -42,18 +42,18 @@ def plot_fragment_colored(ax, fragment):
 
 def plot_fragment_from_df(ax, fragment_df):
     for _, atom in fragment_df.iterrows():
-        if atom.atom_symbol == "O":
-            ax.scatter(atom.atom_x, atom.atom_y, atom.atom_z, c="red", s=30, edgecolor="black")
-        elif atom.atom_symbol == "N":
-            ax.scatter(atom.atom_x, atom.atom_y, atom.atom_z, c="blue", s=30, edgecolor="black")
-        elif atom.atom_symbol == "C":
-            ax.scatter(atom.atom_x, atom.atom_y, atom.atom_z, c="black", s=30, edgecolor="black")
-        elif atom.atom_symbol == "I":
-            ax.scatter(atom.atom_x, atom.atom_y, atom.atom_z, c="orchid", s=30, edgecolor="black")
-        elif "aH" in atom.atom_label:
-            ax.scatter(atom.atom_x, atom.atom_y, atom.atom_z, c="fuchsia", s=50, edgecolor="black")
+        if atom.symbol == "O":
+            ax.scatter(atom.x, atom.y, atom.z, c="red", s=30, edgecolor="black")
+        elif atom.symbol == "N":
+            ax.scatter(atom.x, atom.y, atom.z, c="blue", s=30, edgecolor="black")
+        elif atom.symbol == "C":
+            ax.scatter(atom.x, atom.y, atom.z, c="black", s=30, edgecolor="black")
+        elif atom.symbol == "I":
+            ax.scatter(atom.x, atom.y, atom.z, c="orchid", s=30, edgecolor="black")
+        elif "aH" in atom.label:
+            ax.scatter(atom.x, atom.y, atom.z, c="fuchsia", s=50, edgecolor="black")
         else:
-            ax.scatter(atom.atom_x, atom.atom_y, atom.atom_z, c="pink", s=30, edgecolor="black")
+            ax.scatter(atom.x, atom.y, atom.z, c="pink", s=30, edgecolor="black")
 
     return ax
 
@@ -106,9 +106,9 @@ def plot_vdw_spheres(avg_fragment, ax, color, extra=0):
 
         theta, phi = np.mgrid[0:2*np.pi:20j, 0:np.pi:10j]
 
-        x = r * np.sin(phi) * np.cos(theta) + atom.atom_x
-        y = r * np.sin(phi) * np.sin(theta) + atom.atom_y
-        z = r * np.cos(phi) + atom.atom_z
+        x = r * np.sin(phi) * np.cos(theta) + atom.x
+        y = r * np.sin(phi) * np.sin(theta) + atom.y
+        z = r * np.cos(phi) + atom.z
 
         sphere = ax.plot_surface(x, y, z, color=color, alpha=0.2, linewidth=0)
         spheres.append(sphere)
