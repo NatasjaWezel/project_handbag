3e1220356f3255e0232ff70efd3ff3c816b4b297
Natasja Wezel <natasjawezel@hotmail.com>
2020-12-11 15:41:54 +0100

    Add if for methyl (RCOMe), cleanup

--- code/align_kabsch.py
+++ code/align_kabsch.py
@@ -12,8 +12,10 @@ import time
 import numpy as np
 from tqdm import tqdm
 
+from classes.Settings import Settings
 from helpers.alignment_helpers import (alignment_dict, calc_rmse, kabsch_align, perform_rotations,
                                        perform_translation, read_coord_file, read_raw_data)
+from helpers.geometry_helpers import get_dihedral_and_h
 
 
 def main():
@@ -26,18 +28,55 @@ def main():
 
     filename = sys.argv[1]
 
-    title = filename.rsplit('\\')[-1].rsplit('.', 1)[0]
-    central = title.split("_")[0]
-    contact = title.split("_")[1]
+    settings = Settings(filename)
+
+    do_kabsch_align(filename, settings)
+
+    t1 = time.time() - t0
+
+    print("Duration: %.2f s." % t1)
+
+
+def do_kabsch_align(filename, settings):
+    central = settings.central_group_name
+    contact = settings.contact_group_name
 
     # read part of the datafile first for preparation
     no_atoms, no_atoms_central, label_list = read_coord_file(filename)
 
+    alignment = alignment_dict(central_group_name=central)
+
+    # rename label for R group to 'R'
+    if alignment['R']:
+        label_list[label_list.index(alignment['R'])] = 'R'
+        old_R_label = alignment['R']
+        alignment['R'] = 'R'
+
+        for key, value in alignment.items():
+            if value == old_R_label:
+                alignment[key] = 'R'
+
     data, structures = read_raw_data(filename, no_atoms)
     fragments, data = prepare_data(data, no_atoms, label_list)
-    
-    alignment = alignment_dict(central_group_name=filename.rsplit('\\')[-1].rsplit('.', 1)[0]
-                               .rsplit('_aligned', 1)[0].split("_")[0])
+
+    # TODO: make RCOMe general (if making_me_model)
+    # Throw away H's in Methyl groups (because they fuck up alignment)
+    if central == "RCOMe":
+        methyl_model = get_dihedral_and_h()
+        h1, h2, h3 = methyl_model['h1'], methyl_model['h2'], methyl_model['h3']
+
+        old_length = len(data)
+        data = data[((data.label != h1) & (data.label != h2) & (data.label != h3))]
+
+        assert (old_length - len(data)) % 3 == 0, 'Throwing away Hs for methyl does not work'
+
+        # distract hydrogens from amounts
+        no_atoms -= 3
+        no_atoms_central -= 3
+
+        label_list.remove(methyl_model['h1'])
+        label_list.remove(methyl_model['h2'])
+        label_list.remove(methyl_model['h3'])
 
     # restructure df to matrix
     data_xyz_matrix = np.array([np.array(data.x), np.array(data.y), np.array(data.z)]).T
@@ -54,6 +93,8 @@ def main():
         A[:, 2] *= -1
         structures.loc[structures.index == 0, 'mirrored'] = True
 
+    data_xyz_matrix[:no_atoms] = A
+
     A = data_xyz_matrix[0:no_atoms_central]
     n = A.shape[0]
 
@@ -66,14 +107,14 @@ def main():
         B_total_2 = kabsch_align(A, B_central, B_total, n)
 
         # invert if necessary
-        if B_total_2[:,2].mean() < 0:
+        if B_total_2[:, 2].mean() < 0:
             # mirror by switching signs of z coordinate
-            B_total_2[:,2] *= -1
+            B_total_2[:, 2] *= -1
             structures.loc[structures.index == i, 'mirrored'] = True
 
         # calculate and save error
         rmse = calc_rmse(A, B_total_2[:no_atoms_central], n)
-        structures.loc[structures.index == i, 'rmse',] = rmse
+        structures.loc[structures.index == i, 'rmse'] = rmse
 
         # put back into overall matrix
         data_xyz_matrix[i * no_atoms: (i + 1) * no_atoms] = B_total_2
@@ -87,15 +128,11 @@ def main():
     data = data.reindex(['fragment_id', '_id', 'symbol', 'label', 'x', 'y', 'z'], axis=1)
 
     # save as csv
-    structures.to_csv('results/' + central + '/' + central + "_" + contact + "_vdw.5/"\
+    structures.to_csv('results/' + central + '/' + central + "_" + contact + "_vdw.5/"
                       + central + "_" + contact + "_kabsch_structures.csv", index=False)
 
-    data.to_csv('results/' + central + '/' + central + "_" + contact + "_vdw.5/"\
+    data.to_csv('results/' + central + '/' + central + "_" + contact + "_vdw.5/"
                 + central + "_" + contact + "_kabsch_aligned.csv", index=False)
-
-    t1 = time.time() - t0
-
-    print("Duration: %.2f s." % t1)
 
 
 def prepare_data(data, no_atoms, label_list):
