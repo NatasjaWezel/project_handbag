56e48a14e2415f8009d24c105afe6997c927f577
Natasja Wezel <natasjawezel@hotmail.com>
2021-01-05 15:36:12 +0100

    started taking things out of 'headers'

--- code/helpers/headers.py
+++ code/helpers/headers.py
@@ -1,11 +1,6 @@
-
-
 CUT_OFF_ZERO = 1e-10
 AXCOLOR = 'lightgoldenrodyellow'
 
-DATADIR = ".\\data\\"
-RESULTSDIR = ".\\results\\"
-RADII_CSV = ".\\data\\radii.csv"
 CENTRAL_GROUPS_CSV = ".\\data\\central_groups.csv"
 COLORS = ["red", "sandybrown", "gold", "chartreuse", "green", "mediumturquoise",
           "dodgerblue", "darkblue", "slateblue", "mediumorchid", "fuchsia"]
