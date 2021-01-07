cd31db957771d4ce8f5d2172a699a676ac931111
Natasja Wezel <natasjawezel@hotmail.com>
2021-01-07 16:18:21 +0100

    split into several functions

--- code/src/scripts/align_kabsch.py
+++ code/src/scripts/align_kabsch.py
@@ -7,17 +7,20 @@
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
 import sys
-
+import os
 import time
 
 import numpy as np
 from classes.Settings import AlignmentSettings
 from constants.paths import CENTRAL_GROUPS_CSV, WORKDIR
+from constants.constants import MAX_DATAFILE_SIZE
 from tqdm import tqdm
 
-from helpers.alignment_helpers import (alignment_dict, calc_rmse, kabsch_align,
+from helpers.general_helpers import split
+
+from helpers.alignment_helpers import (calc_rmse, kabsch_align,
                                        perform_rotations, perform_translation,
-                                       read_coord_file, read_raw_data)
+                                       read_raw_data)
 
 
 def main():
@@ -36,18 +39,56 @@ def main():
     settings.set_central_group_csv(CENTRAL_GROUPS_CSV)
     settings.prepare_alignment()
 
-    do_kabsch_align(settings)
+    split_file_if_too_big(coordinate_file, settings.no_atoms)
+
+    # TODO: count files
+    data, structures, data_matrix, first_fragment = rotate_first_fragment(settings)
+
+    # align all fragments
+    structures, data_matrix = do_kabsch_align(settings, data_matrix, structures, first_fragment)
+
+    # put back into df
+    data_matrix = data_matrix.T
+    x_vec, y_vec, z_vec = data_matrix[0], data_matrix[1], data_matrix[2]
+    data.x, data.y, data.z = x_vec, y_vec, z_vec
+
+    # reindex the data to a more readable format
+    data = data.reindex(['fragment_id', '_id', 'symbol', 'label', 'x', 'y', 'z'], axis=1)
+
+    # get filenames
+    aligned_csv_filename, structures_csv_filename = settings.get_aligned_csv_filenames()
+
+    # save as csv
+    structures.to_csv(structures_csv_filename, index=False)
+    data.to_csv(aligned_csv_filename, index=False)
 
     t1 = time.time() - t0
 
     print("Duration: %.2f s." % t1)
 
 
-def do_kabsch_align(settings):
+def split_file_if_too_big(filename, no_atoms):
+    # do a filesize check
+    filesize = os.path.getsize(filename)
+
+    # if file bigger than max size, split it into several files
+    if filesize > MAX_DATAFILE_SIZE:
+        print("Splitting original datafile...")
+        output_name_template = filename.rsplit('.', 1)[0] + '_%s.' + filename.rsplit('.', 1)[1]
+
+        # split after fragments, each fragment has no_atoms + 1 header line
+        row_limit = 6e6 - (6e6 % (no_atoms + 1))
+
+        # TODO: make sure it doesn't split if file is small enough
+        split(open(filename), delimiter=',', row_limit=row_limit,
+              output_name_template=output_name_template, output_path='.')
+
+
+def rotate_first_fragment(settings):
     no_atoms = settings.no_atoms
 
     data, structures = read_raw_data(settings.coordinate_data, settings.no_atoms)
-    fragments, data = prepare_data(data, settings)
+    fragments, data = prepare_data(settings, data)
 
     # restructure df to matrix
     data_xyz_matrix = np.array([np.array(data.x), np.array(data.y), np.array(data.z)]).T
@@ -55,32 +96,49 @@ def do_kabsch_align(settings):
     A = data_xyz_matrix[:no_atoms]
     # translate and rotate first fragment onto the origin as for nice viewing
     A = perform_translation(A.copy(), settings.get_index_alignment_atom('center'))
-    A = perform_rotations(A.copy(), [settings.get_index_alignment_atom('center')),
-                                     settings.get_index_alignment_atom('center')])
+    A = perform_rotations(A.copy(), [settings.get_index_alignment_atom('yaxis'),
+                                     settings.get_index_alignment_atom('xyplane')])
 
-    # if mean of z is negative, reflect the whole fragment
-    if A[:, 2].mean() < 0:
-        # mirror by switching signs of z coordinate
-        A[:, 2] *= -1
+    mirrored, A = mirror(A)
+
+    if mirrored:
         structures.loc[structures.index == 0, 'mirrored'] = True
 
     data_xyz_matrix[:no_atoms] = A
 
-    A = data_xyz_matrix[0:no_atoms_central]
+    A = data_xyz_matrix[0:settings.no_atoms_central]
+
+    return data, structures, data_xyz_matrix, A
+
+
+def mirror(matrix):
+    # if mean of z is negative, reflect the whole fragment
+    if matrix[:, 2].mean() < 0:
+        # mirror by switching signs of z coordinate
+        matrix[:, 2] *= -1
+
+        return True, matrix
+
+    return False, matrix
+
+
+def do_kabsch_align(settings, data_matrix, structures, A):
+    no_atoms = settings.no_atoms
+    no_atoms_central = settings.no_atoms_central
+
     n = A.shape[0]
 
     print("Applying Kabsch Algorithm...")
-    for i in tqdm(range(1, fragments)):
-        B_central = data_xyz_matrix[i * no_atoms:no_atoms_central + i * no_atoms]
-        B_total = data_xyz_matrix[i * no_atoms: (i + 1) * no_atoms]
+    for i in tqdm(range(1, settings.get_no_fragments())):
+        B_central = data_matrix[i * no_atoms:no_atoms_central + i * no_atoms]
+        B_total = data_matrix[i * no_atoms: (i + 1) * no_atoms]
 
         # run kabsch, shift frame in data each time
         B_total_2 = kabsch_align(A, B_central, B_total, n)
 
-        # invert if necessary
-        if B_total_2[:, 2].mean() < 0:
-            # mirror by switching signs of z coordinate
-            B_total_2[:, 2] *= -1
+        mirrored, B_total_2 = mirror(B_total_2)
+
+        if mirrored:
             structures.loc[structures.index == i, 'mirrored'] = True
 
         # calculate and save error
@@ -88,39 +146,35 @@ def do_kabsch_align(settings):
         structures.loc[structures.index == i, 'rmse'] = rmse
 
         # put back into overall matrix
-        data_xyz_matrix[i * no_atoms: (i + 1) * no_atoms] = B_total_2
+        data_matrix[i * no_atoms: (i + 1) * no_atoms] = B_total_2
 
-    # put back into df
-    data_xyz_matrix_T = data_xyz_matrix.T
-    x_vec, y_vec, z_vec = data_xyz_matrix_T[0], data_xyz_matrix_T[1], data_xyz_matrix_T[2]
-    data.x, data.y, data.z = x_vec, y_vec, z_vec
-
-    # reindex the data to a more readable format
-    data = data.reindex(['fragment_id', '_id', 'symbol', 'label', 'x', 'y', 'z'], axis=1)
-
-    aligned_csv_filename, structures_csv_filename = settings.get_aligned_csv_filenames()
-
-    # save as csv
-    structures.to_csv(structures_csv_filename, index=False)
-    data.to_csv(aligned_csv_filename, index=False)
+    return structures, data_matrix
 
 
 def prepare_data(settings, data):
     amount_rows = len(data)
 
     # calc amount of fragments
-    fragments = int(amount_rows / settings.no_atoms)
+    no_fragments = int(amount_rows / settings.no_atoms)
+
+    settings.set_no_fragments(no_fragments)
 
     # give each fragment a unique id
-    fragment_ids = range(0, fragments)
+    fragment_ids = range(0, no_fragments)
     fragment_ids = np.repeat(fragment_ids, settings.no_atoms)
     data['fragment_id'] = fragment_ids
 
     # give each atom in each fragment their label from conquest
-    labels = settings.label_list * fragments
+    labels = settings.label_list * no_fragments
     data['label'] = labels
 
-    return fragments, data
+    if not settings.alignment['bin'] == '-':
+        settings.no_atoms -= len(settings.alignment['bin'])
+        settings.no_atoms_central -= len(settings.alignment['bin'])
+
+        data = data[~data.label.isin(settings.alignment['bin'])].reset_index()
+
+    return no_fragments, data
 
 
 if __name__ == "__main__":
