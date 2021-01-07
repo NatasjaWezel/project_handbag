80488a32d7a229b7b80bf0a1cdf06ce4fa99cf6f
Natasja Wezel <natasjawezel@hotmail.com>
2021-01-07 16:19:43 +0100

    data is in src

--- code/src/scripts/constants/paths.py
+++ code/src/scripts/constants/paths.py
@@ -1,7 +1,7 @@
 # setup work dir
 # TODO make this relative again and give input argument from notebook
-WORKDIR = "../"
+WORKDIR = "..\\.."
 
 # set up paths to dirs and files relative to work dir
-RADII_CSV = WORKDIR + "\\data\\radii.csv"
-CENTRAL_GROUPS_CSV = WORKDIR + "\\data\\central_groups.csv"
+RADII_CSV = WORKDIR + "\\src\\data\\radii.csv"
+CENTRAL_GROUPS_CSV = WORKDIR + "\\src\\data\\central_groups.csv"
