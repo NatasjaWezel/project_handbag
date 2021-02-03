e1edfa22603e53add348f859fccceb6a5cd6f55c
Natasja Wezel <natasjawezel@hotmail.com>
2021-02-03 22:29:57 +0100

    Make paths relative again

--- code/src/scripts/constants/paths.py
+++ code/src/scripts/constants/paths.py
@@ -1,7 +1,8 @@
 # setup work dir
-# TODO make this relative again and give input argument from notebook
 WORKDIR = "..\\.."
 
 # set up paths to dirs and files relative to work dir
 RADII_CSV = WORKDIR + "\\src\\data\\radii.csv"
 CENTRAL_GROUPS_CSV = WORKDIR + "\\src\\data\\central_groups.csv"
+METHYL_CSV = WORKDIR + "\\src\\data\\methylmodel.csv"
+FINGERPRINT_CSV = WORKDIR + "\\src\\data\\fingerprints.csv"
